392bc535ac9d99898537b838c88082e6117511c7
CHOI DAE GEON <chleorjs37@gmail.com>
2022-05-14 12:44:17 +0900

    커밋

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,57 @@
           :time-to="19 * 60"  
           :disable-views="['years', 'year', 'month','day']" 
           hide-view-selector
-          hide-title-bar>
+          hide-title-bar
+          :special-hours="specialHours">
   </VueCal>
 </template>
 
 <script>
+import { reactive } from 'vue';
 import VueCal from 'vue-cal';
 import 'vue-cal/dist/vuecal.css';
+
+//vue calendar
+
 export default {
   components:{
-    VueCal
+    VueCal,
   },
   name: 'App',
+  setup(){
+    const dailyHours = { from: 9 * 60, to: 18 * 60, class: 'business-hours' };
+    const specialHours = reactive({
+      1: dailyHours,
+      2: dailyHours,
+      3: [
+        { from: 9*60, to: 12 * 60, class: 'business-hours' },
+        { from: 14 * 60, to: 18 * 60, class: 'business-hours' }
+      ],
+      4: dailyHours,
+      5: dailyHours
+    });
+    
+    return{
+      specialHours
+    }
+  }
 }
 </script>
 
 <style>
+.business-hours {
+  background-color: rgba(255, 255, 0, 0.2);
+  border: solid rgba(255, 210, 0, 0.6);
+  border-width: 2px 0;
+}
+
+/* 현재 상태는 다 none 이나 색 없애기 */
+.vuecal__now-line{
+  display: none;
+}
+
+/* No Event 글 없애기 */
+.vuecal__no-event{
+  display: none;
+}
 </style>
