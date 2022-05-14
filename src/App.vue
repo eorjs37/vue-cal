12f307136e8e51cac7836d17b748153a7ea28f23
CHOI DAE GEON <chleorjs37@gmail.com>
2022-05-14 16:52:17 +0900

    커밋

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
   </h1>
   <VueCal :time-from="8 * 60" 
           :time-to="19 * 60"  
-          :disable-views="['years', 'year', 'month','day']" 
+          :disable-views="['years', 'year', 'month','day']"
+          :time-cell-height="90"
           hide-view-selector
           hide-title-bar
-          :special-hours="specialHours">
+          :events="events">
   </VueCal>
 </template>
 
@@ -24,20 +25,24 @@ export default {
   },
   name: 'App',
   setup(){
-    const dailyHours = { from: 9 * 60, to: 18 * 60, class: 'business-hours' };
-    const specialHours = reactive({
-      1: dailyHours,
-      2: dailyHours,
-      3: [
-        { from: 9*60, to: 12 * 60, class: 'business-hours' },
-        { from: 14 * 60, to: 18 * 60, class: 'business-hours' }
-      ],
-      4: dailyHours,
-      5: dailyHours
-    });
+    const events = reactive([
+        {
+           start: '2022-05-14 10:10',
+           end: '2022-05-14 11:10',
+           content: '불가 <br> 60분',
+           class: 'is_next_class'
+        },
+        {
+           start: '2022-05-14 11:10',
+           end: '2022-05-14 12:10',
+           content: '불가 <br> 60분',
+           class: 'notallowd_class'
+        }
+      ]); 
+   
     
     return{
-      specialHours
+      events
     }
   }
 }
@@ -50,6 +55,45 @@ export default {
   border-width: 2px 0;
 }
 
+.notallowd_class{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: gray;
+  border-radius: 7px;
+  color: #fff;
+}
+
+.is_next_class{
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: gray;
+  border-bottom: 1px solid #eee;
+  border-radius: 7px;
+}
+
+
+/*================= vue-cal 관련 css custom =================*/
+
+.vuecal{
+  background-color: #eee;
+  box-shadow: none;
+}
+
+.vuecal__cell:before{
+  border: none;
+}
+
+.vuecal__cell--selected{
+  background: none;
+}
+
+.vuecal__weekdays-headings{
+  border-bottom: none;
+}
+
 /* 현재 상태는 다 none 이나 색 없애기 */
 .vuecal__now-line{
   display: none;
@@ -59,4 +103,24 @@ export default {
 .vuecal__no-event{
   display: none;
 }
+
+/* 예약된 시간표 색 숨기기 */
+.vuecal__event-time {
+  display: none;
+}
+/* 시간표 시간 cell */
+.vuecal__time-cell{
+  display: flex;
+  justify-content: center;
+}
+.vuecal__time-cell-label{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.vuecal__time-column .vuecal__time-cell-line:before {
+  border: none;
+}
+
 </style>
