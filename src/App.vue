454c801fafe3f2d1d9f08728714c0f37671ad11a
maxgunchoi <chleorjs37@gmail.com>
2022-05-18 15:37:34 +0900

    커밋

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,27 @@
           :time-cell-height="60"
           hide-view-selector
           hide-title-bar
+          hide-weekends
           locale="ko"
           :time-step="30"
           :events="events">
 
           <template v-slot:weekday-heading="{ heading }" class="week_head">
              <div> 
-               <span class="circle"></span>
+               <span class="circle" :class="currentDate === filterDate(heading.date)  ?  'have_class' : 'not_class'"></span>
              </div>
-             <span class="day">
+             <span class="day" :class="currentDate === filterDate(heading.date)  ?  'active' : ''">
                {{ heading.label }}
              </span>
-             <span class="date">
-               20
+             <span class="date" :class="currentDate === filterDate(heading.date)  ?  'active' : ''">
+               {{  filterDate(heading.date)  }}
              </span>
           </template>
   </VueCal>
 </template>
 
 <script>
-import { onMounted, reactive } from 'vue';
+import {  onMounted, reactive, ref } from 'vue';
 import VueCal from 'vue-cal';
 import 'vue-cal/dist/i18n/ko.js';
 import 'vue-cal/dist/vuecal.css';
@@ -43,37 +44,55 @@ export default {
         {
            start: '2022-05-16 10:10',
            end: '2022-05-16 11:10',
-           content: '불가 <br> 60분',
-           class: 'is_next_class',
+           content: '<span class="status_text">불가</span><span class="minute">(60분)</span>',
+           class: 'impossible_class',
         },
         {
            start: '2022-05-16 11:10',
            end: '2022-05-16 12:10',
-           content: '불가 <br> 60분',
-           class: 'notallowd_class'
+           content: '<span class="status_text">불가</span><span class="minute">(60분)</span>',
+           class: 'impossible_class'
         },
         {
            start: '2022-05-17 08:10',
            end: '2022-05-17 12:10',
-           content: '불가 <br> 240분',
-           class: 'is_next_class'
+           content: '<span class="status_text">불가</span><span class="minute">(240분)</span>',
+           class: 'impossible_class'
         },
         {
            start: '2022-05-17 12:10',
            end: '2022-05-17 13:05',
-           content: '불가 <br> 55분',
-           class: 'notallowd_class'
+           content: '<span class="status_text">불가</span><span class="minute">(55분)</span>',
+           class: 'impossible_class'
         },
-      ]); 
+        {
+           start: '2022-05-18 11:30',
+           end: '2022-05-18 11:55',
+           content: '<span class="status_text">예약</span><span class="minute">(25분)</span>',
+           class: 'possible_class'
+        },
+      ]);
+
+      const currentDate = ref(new Date().getDate());
+
+      const filterDate = (val) =>{
+        const date = new Date(val);
+
+        return date.getDate();
+      }
 
 
       onMounted(()=>{
-        console.log(events);
+        events.forEach((val)=>{
+          val['date'] = new Date(val.start).getDate();
+        })
       })
    
     
     return{
-      events
+      events,
+      currentDate,
+      filterDate
     }
   }
 }
@@ -87,31 +106,8 @@ export default {
 }
 body{
   font-family: 'Noto Sans KR', sans-serif;
+  margin: 0;
 }
-.business-hours {
-  background-color: rgba(255, 255, 0, 0.2);
-  border: solid rgba(255, 210, 0, 0.6);
-  border-width: 2px 0;
-}
-
-.notallowd_class{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: gray;
-  color: #fff;
-}
-
-.is_next_class{
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: gray;
-  /* border-radius: 7px; */
-  border-bottom: 5px solid rgba(0, 128, 0, 0.459);
-}
-
 
 /*================= vue-cal css custom =================*/
 
@@ -130,12 +126,24 @@ body{
   flex-direction: column; 
 }
 
+.vuecal__weekdays-headings{
+  padding-right: 0;
+}
+
+.vuecal__cell{
+  padding: 0 4px;
+}
+
 .vuecal__cell:before{
   border: none;
 }
 
 .vuecal__cell--selected{
   background: none;
+}
+
+.vuecal__cell--today{
+  background:none;
 }
 
 .vuecal__weekdays-headings{
@@ -161,14 +169,54 @@ body{
 .vuecal__time-cell{
   display: flex;
   justify-content: center;
+  
 }
+
 .vuecal__time-cell-label{
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 13px;
+  font-weight: 500;
+  color: #1f1f1f;
 }
 
 /*================= vue-cal 관련 css custom =================*/
+
+.business-hours {
+  background-color: rgba(255, 255, 0, 0.2);
+  border: solid rgba(255, 210, 0, 0.6);
+  border-width: 2px 0;
+}
+
+.impossible_class{
+  color: #808080;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color:  #e8e8e8;
+  border: 1px solid #fff;
+}
+
+.possible_class{
+  color: #1f1f1f;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color:  #fae9c3;;
+  border: 1px solid #fff;
+}
+
+
+.wait_class{
+  color: #1f1f1f;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d5f1cd;
+  border: 1px solid #fff;
+}
+
 .status{
   height: 10px;
 }
@@ -177,7 +225,14 @@ body{
   width: 6px;
   height: 6px;
   border-radius: 50%;
+}
+
+.have_class{
   background-color: #e76d24;
+}
+
+.not_class{
+  background-color: transparent;
 }
 
 .day{
@@ -194,4 +249,20 @@ body{
   line-height: 17px;
   color: #a1a1a1;
 }
+
+.active{
+  color: #e76d24;
+}
+
+.status_text{
+  font-size: 14px;
+  display: block;
+}
+
+
+.minute{
+  font-size: 10px;
+  display: block;
+}
+
 </style>
