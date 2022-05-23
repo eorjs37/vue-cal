c5ad4e6fe0027e4bbad0b66f63e7e9838891ac74
maxgunchoi <chleorjs37@gmail.com>
2022-05-23 10:03:46 +0900

    커밋

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <h1>
     Vue-cal
   </h1>
-  <button @click="changeView()">일별</button>
+  <button @click="changeView('day')">일별</button>
+  <button @click="changeView('week')">주별</button>
   <button @click="stickySplitLabels = !stickySplitLabels">Change</button>
   <VueCal :time-from="9 * 60" 
           :time-to="19 * 60"  
@@ -14,8 +15,9 @@
           locale="ko"
           :time-step="30"
           :events="events"
-          :split-days="customDaySplitLabels"
+          :split-days="splitLabels"
           :sticky-split-labels="stickySplitLabels"
+          @cell-click="modal()"
           ref="vuecal">
 
           <template v-slot:weekday-heading="{ heading }" class="week_head">
@@ -34,6 +36,13 @@
             <strong :style="`color: ${split.color}`">{{ split.label }}</strong>
           </template>
   </VueCal>
+
+  <h1 class="title">달력</h1>
+  <div>
+    <VueCal class="cal" :time="false" active-view="month" :disable-views="['years', 'year','week','day']">
+
+    </VueCal>
+  </div>
 </template>
 
 <script>
@@ -51,42 +60,59 @@ export default {
   setup(){
     const vuecal = ref(null);
     const stickySplitLabels = ref(true);
+    
+    const splitLabels = reactive([]);
+    const customDaySplitLabels = reactive(
+      [
+        { id:1, label: 'John' },
+        { id:2, label: 'Tom'},
+        { id:3, label: 'Kate'},
+        { id:4, label: 'Jess' }
+      ]
+    );
+
     const events = reactive([
         {
-           start: '2022-05-16 09:00',
-           end: '2022-05-16 10:10',
+           start: '2022-05-23 09:00',
+           end: '2022-05-23 10:10',
            content: '<span class="status_text">불가</span><span class="minute">(60분)</span>',
            class: 'impossible_class',
+           split: 1
         },
         {
-           start: '2022-05-16 11:10',
-           end: '2022-05-16 12:10',
+           start: '2022-05-23 11:10',
+           end: '2022-05-23 12:10',
            content: '<span class="status_text">불가</span><span class="minute">(60분)</span>',
-           class: 'impossible_class'
+           class: 'impossible_class',
+           split: 1
         },
         {
-           start: '2022-05-17 08:10',
-           end: '2022-05-17 12:10',
+           start: '2022-05-24 08:10',
+           end: '2022-05-24 12:10',
            content: '<span class="status_text">불가</span><span class="minute">(240분)</span>',
-           class: 'impossible_class'
+           class: 'impossible_class',
+           split: 2
         },
         {
-           start: '2022-05-17 12:10',
-           end: '2022-05-17 13:05',
+           start: '2022-05-24 12:10',
+           end: '2022-05-24 13:05',
            content: '<span class="status_text">불가</span><span class="minute">(55분)</span>',
-           class: 'impossible_class'
+           class: 'impossible_class',
+           split: 2
         },
         {
-           start: '2022-05-18 08:30',
-           end: '2022-05-18 11:55',
+           start: '2022-05-25 08:30',
+           end: '2022-05-25 11:55',
            content: '<span class="status_text">예약</span><span class="minute">(25분)</span>',
-           class: 'possible_class'
+           class: 'possible_class',
+           split: 3  
         },
         {
-           start: '2022-05-19 08:30',
-           end: '2022-05-19 11:55',
+           start: '2022-05-26 08:30',
+           end: '2022-05-26 11:55',
            content: '<span class="status_text">예약</span><span class="minute">(25분)</span>',
-           class: 'possible_class'
+           class: 'possible_class',
+           split: 3
         },
       ]);
 
@@ -98,38 +124,39 @@ export default {
         return date.getDate();
       }
 
-      const customDaySplitLabels = reactive(
-        [
-          { label: 'John', color: 'blue', class: 'split1' },
-          { label: 'Tom', color: 'green', class: 'split2' },
-          { label: 'Kate', color: 'orange', class: 'split3' },
-          { label: 'Jess', color: 'red', class: 'split4' }
-        ]
-      );
-
-      const changeView = () =>{
+      const changeView = (view = 'week') =>{
+        if(view ==='day'){
+          Object.assign(splitLabels,customDaySplitLabels);
+        }
+        else{
+          splitLabels.splice(0);
+        }
+        
         const  { switchView } = vuecal.value;
-        switchView('day',new Date());
+        switchView(view,new Date());
+      }
+
+      const modal = () =>{
+        alert('팝업 클릭');
       }
 
 
       onMounted(()=>{
         events.forEach((val)=>{
           val['date'] = new Date(val.start).getDate();
-        });
-
-        console.log(vuecal.value);
-        
+        }); 
       });
    
     
     return{
       vuecal,
+      splitLabels,
       stickySplitLabels,
       events,
       currentDate,
       filterDate,
       changeView,
+      modal,
       customDaySplitLabels
     }
   }
@@ -147,6 +174,9 @@ body{
   margin: 0;
 }
 
+.title{
+  text-align: center;
+}
 /*================= vue-cal css custom =================*/
 
 .vuecal{
@@ -220,6 +250,17 @@ body{
   color: #1f1f1f;
 }
 
+.vuecal__cell-split.impossible_class{
+  background-color: #e8e8e8;
+}
+
+.vuecal__cell-split.possible_class{
+  background-color: #fae9c3;
+}
+
+.vuecal__cell-split.wait_class{
+  background-color: #d5f1cd;
+}
 /*================= vue-cal 관련 css custom =================*/
 
 .business-hours {
@@ -304,4 +345,8 @@ body{
   display: block;
 }
 
+
+.cal{
+  background-color: #fff;
+}
 </style>
